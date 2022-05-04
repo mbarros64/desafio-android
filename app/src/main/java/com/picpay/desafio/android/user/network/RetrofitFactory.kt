ad7836e7314aca0a3206730b9c92af29a911f49f
Matheus Prestes <mathbar@motorola.com>
2022-05-04 15:40:43 -0400

    avoid magic strings.

--- app/src/main/java/com/picpay/desafio/android/user/network/RetrofitFactory.kt
+++ app/src/main/java/com/picpay/desafio/android/user/network/RetrofitFactory.kt
@@ -6,9 +6,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitFactory {
+    private const val API_KEY = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
     fun <T> build(apiClass: Class<T>): T {
         return Retrofit.Builder()
-            .baseUrl("https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/")
+            .baseUrl(API_KEY)
             .client(OkHttpClient.Builder().build())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
