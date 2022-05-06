a90a9cb2ba93b6f3f296a23065723367864ef7b4
Matheus Prestes <mathbar@motorola.com>
2022-05-06 15:27:08 -0400

    Implement runner test and live data testing.

---
+++ app/src/test/java/com/picpay/desafio/android/extension/LiveDataExtensionKtTest.kt
@@ -0,0 +1,40 @@
+package com.picpay.desafio.android.extension
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.picpay.desafio.android.extensions.updateAsync
+import org.junit.Rule
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class LiveDataExtensionKtTest {
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+
+    @Test
+    fun `Should update live data value with async update`() {
+        val oldData = LiveDataTestClass("old")
+        val liveData = MutableLiveData(oldData)
+
+        liveData.updateAsync { copy(text = "new") }
+
+        val expected = LiveDataTestClass("new")
+        liveData.observeForever {
+            assertEquals(expected, it)
+        }
+    }
+
+    @Test
+    fun `Should do nothing when updating live data with no value`() {
+        val liveData = MutableLiveData<LiveDataTestClass>()
+
+        liveData.updateAsync { copy(text = "new") }
+
+        assertNull(liveData.value)
+    }
+
+    private data class LiveDataTestClass(val text: String)
+}
